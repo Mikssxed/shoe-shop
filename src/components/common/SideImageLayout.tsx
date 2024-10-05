9a884a615c422c8728627113d958bd6b5c3c872b
Vladyslav Kovalchuk <81990282+vladkvlchk@users.noreply.github.com>
2024-10-05 18:09:03 +0300

    fix: dublicated import (#97)

--- src/components/common/SideImageLayout.tsx
+++ src/components/common/SideImageLayout.tsx
@@ -6,7 +6,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import { stylingConstants } from '@/lib/constants/themeConstants';
-import { useState } from 'react';
 import Testimonial from './Testimonial';
 
 interface ISideImageLayoutProps {
