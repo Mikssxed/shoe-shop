bb17858875a10d9f105091fa019e1c7af4095f21
A. Talha Ercan <109243387+ATErcan@users.noreply.github.com>
2024-10-15 15:30:10 +0200

    [ES129-refactor/new-sidebar-links] - add new links to profile sidebar (#109)

--- src/lib/config/profileSidebarConfig.ts
+++ src/lib/config/profileSidebarConfig.ts
@@ -1,4 +1,11 @@
-import { BagTick, Logout, Setting2 } from 'iconsax-react';
+import {
+  BagTick,
+  HeartSearch,
+  I3DCubeScan,
+  Logout,
+  SearchStatus1,
+  Setting2,
+} from 'iconsax-react';
 import { ProfileSidebar } from '../types/profile-sidebar-types';
 
 export const profileSidebarData: ProfileSidebar[] = [
@@ -10,12 +17,31 @@ export const profileSidebarData: ProfileSidebar[] = [
   },
   {
     id: 'ps-2',
+    name: 'Order history',
+    icon: I3DCubeScan,
+    // TODO: change this link if necessary after order history page created
+    path: '/profile/order-history',
+  },
+  {
+    id: 'ps-3',
+    name: 'My Wishlist',
+    icon: HeartSearch,
+    path: '/profile/my-wishlist',
+  },
+  {
+    id: 'ps-4',
+    name: 'Recently Viewed',
+    icon: SearchStatus1,
+    path: '/profile/recently-viewed',
+  },
+  {
+    id: 'ps-5',
     name: 'Settings',
     icon: Setting2,
     path: '/profile/settings',
   },
   {
-    id: 'ps-3',
+    id: 'ps-6',
     name: 'Log out',
     icon: Logout,
     path: '',
