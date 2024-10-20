ba6671b6ec91f0fa0ab3ac8e9ffb85fb6facb2ad
A. Talha Ercan <109243387+ATErcan@users.noreply.github.com>
2024-10-20 19:07:00 +0200

    [ES168-refactor/wishlist-icon-tooltip] - add tooltip to remove from wishlist icon (#134)

--- src/components/common/WishlistIcon.tsx
+++ src/components/common/WishlistIcon.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Tooltip } from '@mui/material';
 import { HeartSlash } from 'iconsax-react';
 
 import { stylingConstants } from '@/lib/constants/themeConstants';
@@ -11,32 +11,34 @@ export default function WishlistIcon({
   id: number;
 }) {
   return (
-    <Box
-      component="div"
-      sx={{
-        width: '44px',
-        height: '44px',
-        position: 'absolute',
-        top: 10,
-        right: 10,
-        zIndex: '1',
-        p: '10px',
-        bgcolor: '#FFFFFF80',
-        borderRadius: '12px',
-        cursor: 'pointer',
-        '&:hover': {
-          opacity: 0.8,
-          '& svg': {
-            color: stylingConstants.palette.primary.main,
+    <Tooltip title="Remove From Wishlist" placement="top-start">
+      <Box
+        component="div"
+        sx={{
+          width: '44px',
+          height: '44px',
+          position: 'absolute',
+          top: 10,
+          right: 10,
+          zIndex: '1',
+          p: '10px',
+          bgcolor: '#FFFFFF80',
+          borderRadius: '12px',
+          cursor: 'pointer',
+          '&:hover': {
+            opacity: 0.8,
+            '& svg': {
+              color: stylingConstants.palette.primary.main,
+            },
           },
-        },
-        '& svg': {
-          color: stylingConstants.palette.text.primary,
-        },
-      }}
-      onClick={() => handleWishlist(id.toString())}
-    >
-      <HeartSlash size="24" />
-    </Box>
+          '& svg': {
+            color: stylingConstants.palette.text.primary,
+          },
+        }}
+        onClick={() => handleWishlist(id.toString())}
+      >
+        <HeartSlash size="24" />
+      </Box>
+    </Tooltip>
   );
 }
