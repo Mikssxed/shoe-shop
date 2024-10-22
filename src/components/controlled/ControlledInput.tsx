1be4b85fc4d8af4cf5cdb96fc610480cc3e5977f
Vladyslav Kovalchuk <81990282+vladkvlchk@users.noreply.github.com>
2024-10-22 17:03:50 +0300

    [ES-172/BugFix] - wrap ControlledInput with forwardRef (#142)

--- src/components/controlled/ControlledInput.tsx
+++ src/components/controlled/ControlledInput.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ComponentProps, RefObject } from 'react';
+import { ComponentProps, forwardRef } from 'react';
 import { Controller } from 'react-hook-form';
 import { Box, SxProps, Theme } from '@mui/material';
 
@@ -16,27 +16,28 @@ interface IControlledInputProps {
   inputStyle?: SxProps<Theme>;
   type?: 'password' | 'number';
   containerProps?: ComponentProps<typeof Box>;
-  ref?: RefObject<HTMLInputElement>;
 }
 
-const ControlledInput = ({
-  name,
-  control,
-  label,
-  required = false,
-  disabled = false,
-  placeholder,
-  inputStyle,
-  type,
-  containerProps,
-  ref,
-}: IControlledInputProps) => {
-  return (
-    <Controller
-      name={name}
-      control={control}
-      render={({ field: { onChange, value }, fieldState: { error } }) => (
-        <>
+const ControlledInput = forwardRef<HTMLInputElement, IControlledInputProps>(
+  (
+    {
+      name,
+      control,
+      label,
+      required = false,
+      disabled = false,
+      placeholder,
+      inputStyle,
+      type,
+      containerProps,
+    },
+    ref,
+  ) => {
+    return (
+      <Controller
+        name={name}
+        control={control}
+        render={({ field: { onChange, value }, fieldState: { error } }) => (
           <Input
             required={required}
             label={label}
@@ -48,15 +49,17 @@ const ControlledInput = ({
               'data-testid': `${label}-input`,
               ref,
             }}
-            errorMessage={error ? error?.message : undefined}
+            errorMessage={error ? error.message : undefined}
             inputStyle={inputStyle}
             containerProps={containerProps}
             disabled={disabled}
           />
-        </>
-      )}
-    />
-  );
-};
+        )}
+      />
+    );
+  },
+);
+
+ControlledInput.displayName = 'ControlledInput';
 
 export default ControlledInput;
